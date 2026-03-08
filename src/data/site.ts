6f28d8bac5c3fdf7aefb814a9b9b1bcc73b060a8
artemKrivko <krivko219319@gmail.com>
2026-03-08 22:50:49 +0300

    first commit

---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+export const site = {
+  companyName: "Dekolux",
+  region: "Могилёв и Могилёвская область",
+  phoneDisplay: "+375 (29) 632-66-50",
+  phoneHref: "+375296326650",
+  messengers: ["WhatsApp", "Telegram", "Viber"],
+};
