9059524f5739362851b80d57b037d7f220f6d5cf
artemKrivko <krivko219319@gmail.com>
2026-03-09 02:33:39 +0300

    add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: "https://dekolux.vercel.app",
+
   output: "server",
   adapter: vercel({}),
+
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
   }
