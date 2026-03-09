45ebeb6d2495cd001d1de8cc1f97e2c2e69132ea
artemKrivko <krivko219319@gmail.com>
2026-03-10 02:51:38 +0300

    add city pages and internal links for SEO

---
+++ src/data/city-pages.ts
@@ -0,0 +1,44 @@
+export const cityPages = [
+  {
+    slug: "bobruysk",
+    city: "Бобруйске",
+    cityTitle: "Бобруйск",
+    district: "Бобруйске и Бобруйском районе",
+  },
+  {
+    slug: "osipovichi",
+    city: "Осиповичах",
+    cityTitle: "Осиповичи",
+    district: "Осиповичах и Осиповичском районе",
+  },
+  {
+    slug: "shklov",
+    city: "Шклове",
+    cityTitle: "Шклов",
+    district: "Шклове и Шкловском районе",
+  },
+  {
+    slug: "chausy",
+    city: "Чаусах",
+    cityTitle: "Чаусы",
+    district: "Чаусах и Чаусском районе",
+  },
+  {
+    slug: "gorki",
+    city: "Горках",
+    cityTitle: "Горки",
+    district: "Горках и Горецком районе",
+  },
+  {
+    slug: "krugloe",
+    city: "Круглом",
+    cityTitle: "Круглое",
+    district: "Круглом и Круглянском районе",
+  },
+  {
+    slug: "klimovichi",
+    city: "Климовичах",
+    cityTitle: "Климовичи",
+    district: "Климовичах и Климовичском районе",
+  },
+];
