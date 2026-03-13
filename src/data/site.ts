544f79a930f34a557f66000a621159e0fb88e0b6
artemKrivko <krivko219319@gmail.com>
2026-03-14 01:45:02 +0300

    replace old phone number in all files

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,6 @@ export const site = {
   companyName: "Dekolux",
   region: "Могилёв и Могилёвская область",
   phoneDisplay: "+375 (29) 632-66-50",
-  phoneHref: "+375296326650",
+  phoneHref: "+375293326650",
   messengers: ["WhatsApp", "Telegram", "Viber"],
 };
