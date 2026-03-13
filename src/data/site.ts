5f5932dc4199007d9ed54de0b06e7bde9e737442
artemKrivko <krivko219319@gmail.com>
2026-03-14 01:51:42 +0300

    update phone number everywhere

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   companyName: "Dekolux",
   region: "Могилёв и Могилёвская область",
-  phoneDisplay: "+375 (29) 632-66-50",
+  phoneDisplay: "+375 (29) 332-66-50",
   phoneHref: "+375293326650",
   messengers: ["WhatsApp", "Telegram", "Viber"],
 };
